efe9a79b1ceb320ad9ae5edc9f617ac5035c4ff2
F22 Labs <amarnath@f22labs.com>
2023-02-24 13:56:15 +0530

    new commit with fetch photos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import InstagramAPI from 'instagram-private-api';
+
 import "./App.css";
 
 function App() {
@@ -7,21 +7,28 @@ function App() {
   const [userPhotos, setUserPhotos] = useState([]);
 
   const handleLogin = async () =>{
-    const ig = new InstagramAPI.IgApiClient();
-    ig.state.generateDevice(import.meta.env.VITE_IG_USERNAME);
 
-    const auth = await ig.account.login(import.meta.env.VITE_IG_USERNAME, import.meta.env.VITE_IG_PASSWORD);
+    const tokenResponse = await fetch(
+      `https://api.instagram.com/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}
+      &redirect_uri=${import.meta.env.VITE_REDIRECT_URI}
+      &scope=user_profile,user_media
+      &response_type=code`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        }
+      }
+    );
+    const tokenData = await tokenResponse.json();
     setIsLoggedIn(true);
 
-    const feed = await ig.feed.user(auth.pk);
-    const items = await feed.items();
-    setUserPhotos(items);
-  }
+    
+    console.log(tokenData);
+  };
   
   return (
     <div className="w-full h-screen">
-      <p>{import.meta.env.VITE_IG_USERNAME}</p>
-      <p>{import.meta.env.VITE_IG_PASSWORD}</p>
       <h1 className="p-3 text-center text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 to-yellow-200 text-lg">
         Welcome To Insta-Post
       </h1>
@@ -30,14 +37,7 @@ function App() {
           Login with Instagram
         </button>
       </div>}
-
-      {isLoggedIn && (
-        <div>
-          {userPhotos.map((photo) => (
-            <img key={photo.id} src={photo.image_versions2.candidates[0].url} />
-          ))}
-        </div>
-      )}
+      
     </div>
   );
 }
