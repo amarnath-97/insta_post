6f11d21a7f05da757ed4d24144c708ea7d19dda8
F22 Labs <amarnath@f22labs.com>
2023-02-24 19:13:53 +0530

    updated code

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "./Image";
 
 const ImageGallery = ({images}) => {
-  return <div className="image-gallery w-full h-auto grid grid-cols-3 gap-2">
+  return <div className="image-gallery w-full h-[80%] mx-0 my-auto grid grid-cols-4 justify-center gap-2">
     {images.map(image => (
         <Image image={image}/>
     ))}
