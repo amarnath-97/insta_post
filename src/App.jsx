b2beac1140db228c85af85040fbe26db7aee2537
F22 Labs <amarnath@f22labs.com>
2023-02-24 14:15:07 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,44 @@ import { useState } from "react";
 
 import "./App.css";
 
-import React, { useState } from "react";
-
-const App = () => {
+function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userPhotos, setUserPhotos] = useState([]);
 
-  const handleLogin = async () => {
-    const redirectURI = encodeURIComponent(import.meta.env.VITE_REDIRECT_URI);
-    const authURL = `https://api.instagram.com/oauth/authorize?client_id=YOUR_APP_ID&redirect_uri=${redirectURI}&scope=user_photos&response_type=code`;
-    window.location.href = authURL;
-  };
-
-  const handleAuthCallback = async () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get("code");
+  const handleLogin = async () =>{
 
     const tokenResponse = await fetch(
-      `https://api.instagram.com/oauth/access_token`,
+      `https://api.instagram.com/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}
+      &redirect_uri=${import.meta.env.VITE_REDIRECT_URI}
+      &scope=user_profile,user_media
+      &response_type=code`,
       {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-          client_id: import.meta.env.VITE_CLIENT_ID,
-          client_secret: import.meta.env.VITE_CLIENT_SECRET,
-          grant_type: "authorization_code",
-          redirect_uri: import.meta.env.VITE_REDIRECT_URI,
-          code: code,
-        }),
+        }
       }
     );
     const tokenData = await tokenResponse.json();
     setIsLoggedIn(true);
 
-    const mediaResponse = await fetch(
-      `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url&access_token=${tokenData.access_token}`
-    );
-    const mediaData = await mediaResponse.json();
-    setUserPhotos(mediaData.data);
+    
+    console.log(tokenData);
   };
-
-  if (!isLoggedIn) {
-    if (window.location.search.includes("code")) {
-      handleAuthCallback();
-    } else {
-      return <button onClick={handleLogin}>Login with Instagram</button>;
-    }
-  }
-
+  
   return (
-    <div>
-      {userPhotos.map((photo) => (
-        <img key={photo.id} src={photo.media_url} />
-      ))}
+    <div className="w-full h-screen">
+      <h1 className="p-3 text-center text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 to-yellow-200 text-lg">
+        Welcome To Insta-Post
+      </h1>
+      {!isLoggedIn && <div className="w-full flex justify-center">
+        <button className="p-1 text-white bg-blue-400 m-3 rounded-sm" onClick={()=> handleLogin()}>
+          Login with Instagram
+        </button>
+      </div>}
+      
     </div>
   );
-};
+}
 
 export default App;
-
