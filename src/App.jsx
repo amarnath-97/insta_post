71d22b69fbea6f6f9cc309463172890821386c1f
F22 Labs <amarnath@f22labs.com>
2023-02-24 11:25:14 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen'>
+      <h1 className='text-center text-white bg-pink-500 text-lg'>Welcome To Insta-Post</h1>
+      
+    </div>
+  )
+}
+
+export default App;
