0ad4cc5210e6450bd83b6d5b9c790c462adc3a24
F22 Labs <amarnath@f22labs.com>
2023-02-27 12:55:46 +0530

    remove login button aftet login

--- src/components/PostGallery.jsx
+++ src/components/PostGallery.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Post from "./Post";
 
 const PostGallery = ({images}) => {
-  return <div className="mt-4 image-gallery w-[80%] h-auto mx-auto my-0 grid grid-cols-4 justify-center gap-2">
+  return <div className="mt-6 image-gallery w-[80%] h-auto mx-auto my-0 grid grid-cols-4 justify-center gap-2">
     {images.map(image => (
         <Post image={image}/>
     ))}
